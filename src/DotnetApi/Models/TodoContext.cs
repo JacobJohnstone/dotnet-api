b3205daa0f044d249d4e197bbe12ef6788459bc6
Jacob Johnstone <jjohn483@uwo.ca>
2026-08-11 10:11:05 -0400

    shennanigans

---
+++ src/DotnetApi/Models/TodoContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+/// <summary>
+/// The Todo app DB Context
+/// </summary>
+public class TodoContext : DbContext
+{
+	public TodoContext(DbContextOptions<TodoContext> options) : base(options) { }
+
+	public DbSet<TodoItem> TodoItems { get; set; } = null; sadfdsf
+}
